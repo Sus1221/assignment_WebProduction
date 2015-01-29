01047cd1d38c39138ab7b2d5a54fe660af7a5255
Sus1221 <susanne881221@hotmail.com>
2015-01-29 12:03:37 +0100

    halfway through fixing push-pop-state

--- js/ownJs/main.js
+++ js/ownJs/main.js
@@ -5,13 +5,18 @@ $(function() {
 	$("section.home").show();
 	
 	//Handles class-"active" and visibility of sections according to navchoice-click
-	$("body").on("click", "#headerNavDiv a", function() {
-		$("#headerNavDiv li").removeClass("active");
-		$(this).parent().addClass("active");
-		$("section").hide();
-		console.log($(this).attr("id"));
-		var rightString = $(this).attr("id");
-		$("section."+rightString).show();
+	$("body").on("click","a", function() {
+		var href = $(this).attr("href");
+		console.log(href);
+		if ($(this).parents("#headerNavDiv").length > 0) {
+			$("#headerNavDiv li").removeClass("active");
+			$(this).parent().addClass("active");
+			$("section").hide();
+		
+			var rightString = $(this).attr("href");
+			$("section."+rightString).show();
+		}
+		event.preventDefault();
 	});
 
 });
