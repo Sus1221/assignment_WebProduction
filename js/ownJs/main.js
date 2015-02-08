201e20f0e436bac49fdda536f23a63c215ed1105
Sus1221 <susanne881221@hotmail.com>
2015-02-08 16:55:18 +0100

    working atricle-submit-form again

--- js/ownJs/main.js
+++ js/ownJs/main.js
@@ -5,6 +5,19 @@ $(function() {
 	$("section.home").show();
 	createSelectForArticleMenu();
 	
+		//When submitting a new article
+	$("#newArticleForm").submit(function() {
+		var article = {};
+		// article.parent = $("#newArticleForm select option:selected").text();
+		article.heading = $("#articleHeading").val();
+		article.body = $("#articleBody").val();
+		console.log("article submit:", article);
+		//empty form
+		this.reset();
+		//run to send article-data to db
+		articleToDb(article);
+		return false;
+	});
 });
 
 
