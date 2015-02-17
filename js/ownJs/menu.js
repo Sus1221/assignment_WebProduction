28503df830b6af0e646c1d96b2d0e904f9bd0156
Sus1221 <susanne881221@hotmail.com>
2015-02-17 14:01:41 +0100

    functioning menu including hover, active.

--- js/ownJs/menu.js
+++ js/ownJs/menu.js
@@ -57,49 +57,57 @@ function buildMenu(menusFromDb) {
 	console.log("menusFromDb", menusFromDb);
 	var nav = $("header nav");
 	var mainMenuHtml = $('<ul class="nav nav-pills" id="bottomUlInNav"/>');
+	//add menu item "home"
+	mainMenuHtml.append('<li role="presentation" class="active liNotInSelect liInNavConstant"><a href="home">Hem</a></li>');
 	console.log("mainMenuHtml", mainMenuHtml);
 	//sort menuItems by weight, low to high
 	menusFromDb.sort(function(x,y){
 		return x.weight > y.weight;
 	});
 	console.log("menus after sorting: ", menusFromDb);
+	//while there are menuItems left in menusFromDb
 	while (menusFromDb.length > 0) {
 		var currentLink = menusFromDb[0];
+		//value of parentMlid is either the parent's id or null if no parent exists
 		var parentMlid = currentLink.plid ? "ml_"+currentLink.plid : null;
-
 		//if item has parent and parent doesn't exist yet
 		if (parentMlid && mainMenuHtml.find('li[data-mlid="'+parentMlid+'"]').length === 0) {
+			//remove item[0] and put it at the end of the array
 			menusFromDb.push(menusFromDb.shift());
 			continue;
-		}
-			else {
-			//parent exists
+		//if menuitem's parent exists or menuitem doesn't have a parent
+		}else {
 			var menuLink = $('<li data-mlid="ml_'+currentLink.ml_id+'"><a href="'+currentLink.path+'">'+currentLink.title+'</a></li>');
 			//if item doesn't have parent
 			if(!parentMlid) {
+				//add menu link at level 0
 				mainMenuHtml.append(menuLink);
-			//if item has parent - find it!
+			//if item has parent - find it and add a class
 			} else {
 				mainMenuHtml.find('li[data-mlid="'+parentMlid+'"]').addClass("dropdown");
-				//check if parent does not have an ul
-				if (mainMenuHtml.find('li[data-mlid="'+parentMlid+'"] > ul').length === 0) {
-					//if not make one (create submenu <ul></ul>)
-					var newSubMenu = $('<ul class="dropdown-menu"/>');
-					//append it to parent link 
-					mainMenuHtml.find('li[data-mlid="'+parentMlid+'"]').append(newSubMenu);
-				}
-			mainMenuHtml.find('li[data-mlid="'+parentMlid+'"]').append(menuLink);
+			//If the parent doesn't have an ul-child
+			if (mainMenuHtml.find('li[data-mlid="'+parentMlid+'"] > ul').length === 0) {
+				// make a ul
+				var newSubMenu = $('<ul class="dropdown-menu"/>');
+				//give the parent the ul as a child
+				mainMenuHtml.find('li[data-mlid="'+parentMlid+'"]').append(newSubMenu);
+				mainMenuHtml.find('li[data-mlid="'+parentMlid+'"] > ul').append(menuLink);
+			}else{
+				mainMenuHtml.find('li[data-mlid="'+parentMlid+'"] > ul').append(menuLink);
+			}
 		}
+		//remove item[0] in array
 		menusFromDb.shift();
 		}
 		console.log("menusFromDb", menusFromDb);
 	}
-
-	mainMenuHtml.append('<li role="presentation" class="active liNotInSelect liInNavConstant"><a href="home">Hem</a></li>'+
+	//Always append to ul these ones: "Home", "All articles, "Adminfunctions"
+	mainMenuHtml.append(
               '<li role="presentation" class="liNotInSelect liInNavConstant"><a href="all">Alla artiklar</a></li>'+
               '<li id="showAdminPage" class="liNotInSelect liInNavConstant">'+
                 '<a class="btn btn-default" href="admin">Adminfunktioner <span class="glyphicon glyphicon-cog"></span></a>'+
               '</li>');
+	//In header nav - replace all content with new menu-ul
 	nav.html(mainMenuHtml);
 }
 
